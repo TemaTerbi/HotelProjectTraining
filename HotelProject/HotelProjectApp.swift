ded56972b51ea7f5e060c81a9f75fde61fcb90a1
TemaTerbi <tematerbi@mail.ru>
2023-12-18 23:26:38 +0700

    Initial Commit

---
+++ HotelProject/HotelProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelProjectApp.swift
+//  HotelProject
+//
+//  Created by Артем Соловьев on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
